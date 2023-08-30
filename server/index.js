0f1d9350f238189b3b1a647ea9e3d3f26f337fec
404 <141179113+CmdErr0r@users.noreply.github.com>
2023-08-30 19:16:46 +0000

    last

--- server/index.js
+++ server/index.js
@@ -1,12 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
-dotenv.config();
 
 const app = express();
+dotenv.config();
 
-const port = process.env.PORT || 3000; // Default to 3000 if PORT is not defined in .env
-const host = process.env.HOST || "localhost"; // Default to localhost if HOST is not defined in .env
-
+const port = process.env.PORT || 3000;
+const host = process.env.HOST || "localhost";
 app.listen(port, () => {
     console.log(`Server is running at http://${host}:${port}`);
     console.log("Your GitHub Page: https://cmderr0r.github.io/");
